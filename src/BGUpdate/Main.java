9098d5e0cc7d01bfe7f7e5ba2a15462d5eb5071b
rhyds <rhyds@users.noreply.github.com>
2023-01-17 22:00:57 +0000

    Update Main.java

--- src/BGUpdate/Main.java
+++ src/BGUpdate/Main.java
@@ -3,7 +3,9 @@ package BGUpdate;
 public class Main {
 
     public static void main(String[] args) {
-
+        
+        System.out.println("Ivan smells");
+        
         new ImageGUI();
 
     }
